fe5167bbafe91b711a1aee12810c60a9246bb820
Sasha Garrette <garrette.sasha@gmail.com>
2021-01-05 20:23:22 +0100

    Initial Commit

---
+++ kansloKoll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kansloKoll
+//
+//  Created by Sasha Garrette on 2021-01-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
